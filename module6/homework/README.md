ad07b41966bb2f5dc47624c37e0ae6a1e71a119f
Issa AlBawwab <issa.albawwab.yes@gmail.com>
2025-03-09 21:15:13 +0300

    hw 5

---
+++ module6/homework/README.md
@@ -0,0 +1,21 @@
+## Question 1
+ - docker compose exec redpanda-1 rpk --version
+ - rpk version v24.2.18 (rev f9a22d4430)
+
+## Question 2
+ - docker compose exec redpanda-1 rpk topic create green-trips
+ - TOPIC        STATUS
+    green-trips  OK
+
+## Question 3
+ - True
+
+## Question 4
+ - time taken : 59.39699578285217
+
+## Question 5
+ - ![alt text](image.png)
+ - PULocationID : 7,  DOLocationID : 7, Streak:43
+
+
+
---
+++ module6/homework/session_job.py
@@ -0,0 +1,107 @@
+from pyflink.datastream import StreamExecutionEnvironment
+from pyflink.table import EnvironmentSettings, DataTypes, TableEnvironment, StreamTableEnvironment
+from pyflink.common.watermark_strategy import WatermarkStrategy
+from pyflink.common.time import Duration
+
+def create_events_aggregated_sink(t_env):
+    table_name = 'processed_events_aggregated'
+    sink_ddl = f"""
+        CREATE TABLE {table_name} (
+            window_start TIMESTAMP(3),
+            PULocationID INTEGER,
+            DOLocationID INTEGER,
+            trip_streak BIGINT
+        ) WITH (
+            'connector' = 'jdbc',
+            'url' = 'jdbc:postgresql://postgres:5432/postgres',
+            'table-name' = '{table_name}',
+            'username' = 'postgres',
+            'password' = 'postgres',
+            'driver' = 'org.postgresql.Driver'
+        );
+        """
+    t_env.execute_sql(sink_ddl)
+    return table_name
+
+def create_events_source_kafka(t_env):
+    table_name = "events"
+    pattern = "yyyy-MM-dd HH:mm:ss"
+    source_ddl = f"""
+        CREATE TABLE {table_name} (
+            lpep_pickup_datetime VARCHAR,
+            lpep_dropoff_datetime VARCHAR,
+            PULocationID INTEGER,
+            DOLocationID INTEGER,
+            passenger_count INTEGER,
+            trip_distance DOUBLE,
+            tip_amount DOUBLE,
+            dropoff_timestamp AS TO_TIMESTAMP(lpep_dropoff_datetime, '{pattern}'),
+            WATERMARK FOR dropoff_timestamp AS dropoff_timestamp - INTERVAL '5' SECOND
+        ) WITH (
+            'connector' = 'kafka',
+            'properties.bootstrap.servers' = 'redpanda-1:29092',
+            'topic' = 'green-trips',
+            'scan.startup.mode' = 'latest-offset',
+            'properties.auto.offset.reset' = 'latest',
+            'format' = 'json'
+        );
+        """
+    t_env.execute_sql(source_ddl)
+    return table_name
+
+
+def log_aggregation():
+    # Set up the execution environment
+    env = StreamExecutionEnvironment.get_execution_environment()
+    env.enable_checkpointing(10 * 1000)
+    env.set_parallelism(1)
+
+    # Set up the table environment
+    settings = EnvironmentSettings.new_instance().in_streaming_mode().build()
+    t_env = StreamTableEnvironment.create(env, environment_settings=settings)
+
+    watermark_strategy = (
+        WatermarkStrategy
+        .for_bounded_out_of_orderness(Duration.of_seconds(5))
+        .with_timestamp_assigner(
+            # This lambda is your timestamp assigner:
+            #   event -> The data record
+            #   timestamp -> The previously assigned (or default) timestamp
+            lambda event, timestamp: event[2]  # We treat the second tuple element as the event-time (ms).
+        )
+    )
+    try:
+        # Create Kafka table
+        source_table = create_events_source_kafka(t_env)
+        aggregated_table = create_events_aggregated_sink(t_env)
+
+        t_env.execute_sql(f"""
+            INSERT INTO {aggregated_table}
+            SELECT
+                SESSION_START(dropoff_timestamp, INTERVAL '5' MINUTE) AS window_start,
+                PULocationID,
+                DOLocationID,
+                COUNT(*) AS trip_streak
+            FROM {source_table}
+            GROUP BY
+                SESSION(dropoff_timestamp, INTERVAL '5' MINUTE),
+                PULocationID,
+                DOLocationID
+        """).wait()
+        
+        # t_env.execute_sql(
+        #     f"""
+        #             INSERT INTO {aggregated_table}
+        #             SELECT
+        #                 *
+        #             FROM {source_table}
+        #             """
+        # ).wait()
+
+
+    except Exception as e:
+        print("Writing records from Kafka to JDBC failed:", str(e))
+
+
+if __name__ == '__main__':
+    log_aggregation()
